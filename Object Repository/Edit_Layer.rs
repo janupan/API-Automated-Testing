9e6a1a51252ffc2e53f3a202217c95e726a11fa1
Rikky Salo <salo02061996@gmail.com>
2018-07-13 14:23:43 +0700

    [update]

--- Object Repository/Edit_Layer.rs
+++ Object Repository/Edit_Layer.rs
@@ -8,7 +8,7 @@
    <useRalativeImagePath>false</useRalativeImagePath>
    <httpBody></httpBody>
    <httpBodyContent>{
-  &quot;text&quot;: &quot;{\n    \&quot;url\&quot;: \&quot;test\&quot;,\n    \&quot;http_method\&quot;: \&quot;Get\&quot;,\n  \t\&quot;header\&quot;:\&quot;\&quot;,\n  \t\&quot;body\&quot;:\&quot;\&quot;\n}&quot;,
+  &quot;text&quot;: &quot;{\n   \n}&quot;,
   &quot;contentType&quot;: &quot;application/json&quot;,
   &quot;charset&quot;: &quot;UTF-8&quot;
 }</httpBodyContent>
@@ -25,15 +25,24 @@
       <matchCondition>equals</matchCondition>
       <name>Authorization</name>
       <type>Main</type>
-      <value>Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE1MzEyOTU5NTQsImV4cCI6MTUzMTMxMzk1NCwiZGF0YSI6eyJ1c2VyX2lkIjoiNDY4MTYyMDgzNDE3Mzg1MzY0NCIsImRpdmlzaW9uX2lkIjoiNDY4MTYyMDgzMjU2MzIzOTg1MyIsImxhYmVsX2lkcyI6W10sImRpdmlzaW9uX2xhYmVsX2lkcyI6W10sImNvbXBhbnlfaWQiOiI0NjgxNjIwODMxNjc0MDQ2NDcxIiwicGxhdGZvcm0iOiJkYXNoYm9hcmQiLCJ1c2VyX2xldmVsX2NvZGVfbmFtZSI6ImFkbWluIn19.A5yjdJrAagRJaA5mosutOv3ieeJV46Ti3syWgKIr8R0</value>
+      <value>Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE1MzE0NTM0NDgsImV4cCI6MTUzMTQ3MTQ0OCwiZGF0YSI6eyJ1c2VyX2lkIjoiNDY4MTYyMDgzNDE3Mzg1MzY0NCIsImRpdmlzaW9uX2lkIjoiNDY4MTYyMDgzMjU2MzIzOTg1MyIsImxhYmVsX2lkcyI6W10sImRpdmlzaW9uX2xhYmVsX2lkcyI6W10sImNvbXBhbnlfaWQiOiI0NjgxNjIwODMxNjc0MDQ2NDcxIiwicGxhdGZvcm0iOiJkYXNoYm9hcmQiLCJ1c2VyX2xldmVsX2NvZGVfbmFtZSI6ImFkbWluIn19.suWqGghcbvQgOs4LT2r_DU6l0I29eQmXq-4HuUquafY</value>
    </httpHeaderProperties>
    <migratedVersion>5.4.1</migratedVersion>
    <restRequestMethod>PUT</restRequestMethod>
-   <restUrl>https://quackapipolda-staging.azurewebsites.net/v1/companies/4681620831674046471/layers/4707277326843390270</restUrl>
+   <restUrl>https://quackapipolda-staging.azurewebsites.net/v1/companies/4681620831674046471/layers/4701897037442985173</restUrl>
    <serviceType>RESTful</serviceType>
    <soapBody></soapBody>
    <soapHeader></soapHeader>
    <soapRequestMethod></soapRequestMethod>
    <soapServiceFunction></soapServiceFunction>
+   <verificationScript>import static org.assertj.core.api.Assertions.*
+
+import com.kms.katalon.core.testobject.ResponseObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webservice.verification.WSResponseManager
+
+import groovy.json.JsonSlurper
+import internal.GlobalVariable as GlobalVariable
+</verificationScript>
    <wsdlAddress></wsdlAddress>
 </WebServiceRequestEntity>
