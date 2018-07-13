c99cf2ee5e03e20918adb7f12118d0e437555bc5
januaripanjaitan <januaripanjaitan@192.168.1.190>
2018-07-13 15:53:00 +0700

    Merapikan

--- Object Repository/Post_Valid_Login.rs
+++ Object Repository/Post_Valid_Login.rs
@@ -35,5 +35,14 @@
    <soapHeader></soapHeader>
    <soapRequestMethod></soapRequestMethod>
    <soapServiceFunction></soapServiceFunction>
+   <verificationScript>import static org.assertj.core.api.Assertions.*
+
+import com.kms.katalon.core.testobject.ResponseObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webservice.verification.WSResponseManager
+
+import groovy.json.JsonSlurper
+import internal.GlobalVariable as GlobalVariable
+</verificationScript>
    <wsdlAddress></wsdlAddress>
 </WebServiceRequestEntity>
